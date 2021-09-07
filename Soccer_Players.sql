8facf144ca9a4f52fc3c35f1c726663b7f230fae
Kerem <90256478+keremmor@users.noreply.github.com>
2021-09-07 15:55:06 +0300

    Add files via upload

---
+++ Soccer_Players.sql
@@ -0,0 +1,73 @@
+USE soccer_players;
+    
+INSERT INTO players(
+		first_name,
+        surname,
+        age,
+        team,
+        country,
+        position)
+VALUES(	'David',
+		'Beckham',
+		27,
+        'Manchester United',
+        'England',
+        'Midfielder');
+        
+INSERT INTO players(
+		first_name,
+        surname,
+        age,
+        team,
+        country,
+        position)
+VALUES(	'Thierry',
+		'Henry',
+		29,
+        'Arsenal',
+        'France',
+        'Striker');
+        
+INSERT INTO players(
+		first_name,
+        surname,
+        age,
+        team,
+        country,
+        position)
+VALUES(	'Petr',
+		'Cech',
+		30,
+        'Chelsea',
+        'Czech Republic',
+        'Goalkeeper');  
+
+INSERT INTO players(
+		first_name,
+        surname,
+        age,
+        team,
+        country,
+        position)
+VALUES(	'Jaap',
+		'Stam',
+		32,
+        'Milan',
+        'Netherlands',
+        'Defender');   
+        
+ INSERT INTO players(
+		first_name,
+        surname,
+        age,
+        team,
+        country,
+        position)
+VALUES(	'Tugay',
+		'Kerimoğlu',
+		22,
+        'Blackburn',
+        'Turkey',
+        'Midfielder');    
+        
+        
